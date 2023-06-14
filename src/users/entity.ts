5444802c1f3ef36008629c587135a16dc113c762
prravda <sonofhuman20@hufs.ac.kr>
2023-06-15 00:18:00 +0900

    impl: - added index to `userId` of user entity

--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,8 +1,15 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    Column,
+    Entity,
+    Index,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Keyword } from '../keywords/entity';
 import { USER_CAME_FROM } from './etc/USER_CAME_FROM';
 
 @Entity()
+@Index(['userId'])
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
