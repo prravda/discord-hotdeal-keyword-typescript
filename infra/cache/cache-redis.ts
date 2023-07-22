d210536dc45c0c7c65704f3deca2c57658e29f9b
prravda <sonofhuman20@hufs.ac.kr>
2023-07-22 23:59:58 +0900

    config: - filled env prop in redis and database connection

--- infra/cache/cache-redis.ts
+++ infra/cache/cache-redis.ts
@@ -1,6 +1,8 @@
 import Redis from 'ioredis';
+import { ENV_LIST } from '../env-config';
 
 export const redisConnection = new Redis({
     host: 'cache-redis',
     port: 6379,
+    password: ENV_LIST.REDIS_PASSWORD,
 });
